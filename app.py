d7cc8138d67a16c0fbd32a22c0ec311bf6f5c58d
aditya kumar pinjala <39559919+adityakumarpinjala@users.noreply.github.com>
2020-07-09 15:47:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, request
 import jsonify
 import requests
@@ -6,7 +7,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('random_forest.pk1', 'rb'))
+model = pickle.load(open('random_forest.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
